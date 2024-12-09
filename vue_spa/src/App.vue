c70d647e63a839d3b8e810f78da87d92492d2d2a
pvidal96 <v.pablo1@gmail.com>
2024-12-09 13:36:34 +0100

    imp: add main vue files

---
+++ vue_spa/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <base>
+    <RouterView />
+  </base>
+</template>
+
+<style>
+body {
+  font-size: 16px;
+  font-family: Helvetica, Arial, sans-serif;
+  padding: 1rem;
+}
+
+</style>
